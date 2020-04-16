ce535d9dd614b1221644014e3f4eb7b5c9902db3
Naoaki ONO <nono@is.naist.jp>
2020-04-16 13:31:00 +0900

    daily update

--- prefecture.R
+++ prefecture.R
@@ -8,18 +8,18 @@ pref47 <- fromJSON(file="data/jisx0401-en.json")
 
 fidtable <- read.table("data/fid.txt")
 
-xxx.tested <- matrix(0,nrow=0,ncol=50)
-xxx.infected <- matrix(0,nrow=0,ncol=50)
-xxx.recovered <- matrix(0,nrow=0,ncol=50)
-xxx.dead <- matrix(0,nrow=0,ncol=50)
+pref47.tested <- matrix(0,nrow=0,ncol=50)
+pref47.infected <- matrix(0,nrow=0,ncol=50)
+pref47.recovered <- matrix(0,nrow=0,ncol=50)
+pref47.dead <- matrix(0,nrow=0,ncol=50)
 
-colnames(xxx.tested) <- c("Month","Day",pref47,"Total")
-colnames(xxx.infected) <- c("Month","Day",pref47,"Total")
-colnames(xxx.recovered) <- c("Month","Day",pref47,"Total")
-colnames(xxx.dead) <- c("Month","Day",pref47,"Total")
+colnames(pref47.tested) <- c("Month","Day",pref47,"Total")
+colnames(pref47.infected) <- c("Month","Day",pref47,"Total")
+colnames(pref47.recovered) <- c("Month","Day",pref47,"Total")
+colnames(pref47.dead) <- c("Month","Day",pref47,"Total")
 
-xxx.tmp <- integer(48)
-names(xxx.tmp) <- c(names(pid),"総計")
+pref47.tmp <- integer(48)
+names(pref47.tmp) <- c(names(pid),"総計")
 
 for(aa in 1:nrow(fidtable)){
     mm <- fidtable[aa,1]
@@ -44,34 +44,34 @@ for(aa in 1:nrow(fidtable)){
         j_dead <- 9 # which(colnames(tmp)=="うち死亡")
     }
 
-    xxx.tmp[] <- 0
-    xxx.tmp[pii] <- tmp[iii,2]
-    xxx.tmp[48] <- tmp[nii,2]
-    if(any(is.na(xxx.tmp))){
-        print(xxx.tmp)
+    pref47.tmp[] <- 0
+    pref47.tmp[pii] <- tmp[iii,2]
+    pref47.tmp[48] <- tmp[nii,2]
+    if(any(is.na(pref47.tmp))){
+        print(pref47.tmp)
     }
-    xxx.infected <- rbind(xxx.infected, c(mm,dd,xxx.tmp))
+    pref47.infected <- rbind(pref47.infected, c(mm,dd,pref47.tmp))
 
-    xxx.tmp[] <- 0
-    xxx.tmp[pii] <- tmp[iii,j_recovered]
-    xxx.tmp[48] <- tmp[nii,j_recovered]
-    if(any(is.na(xxx.tmp))){
-        print(xxx.tmp)
+    pref47.tmp[] <- 0
+    pref47.tmp[pii] <- tmp[iii,j_recovered]
+    pref47.tmp[48] <- tmp[nii,j_recovered]
+    if(any(is.na(pref47.tmp))){
+        print(pref47.tmp)
     }
-    xxx.recovered <- rbind(xxx.recovered, c(mm,dd,xxx.tmp))
+    pref47.recovered <- rbind(pref47.recovered, c(mm,dd,pref47.tmp))
 
-    xxx.tmp[] <- 0
-    xxx.tmp[pii] <- tmp[iii,j_dead]
-    xxx.tmp[48] <- tmp[nii,j_dead]
-    if(any(is.na(xxx.tmp))){
-        print(xxx.tmp)
+    pref47.tmp[] <- 0
+    pref47.tmp[pii] <- tmp[iii,j_dead]
+    pref47.tmp[48] <- tmp[nii,j_dead]
+    if(any(is.na(pref47.tmp))){
+        print(pref47.tmp)
     }
-    xxx.dead <- rbind(xxx.dead, c(mm,dd,xxx.tmp))
+    pref47.dead <- rbind(pref47.dead, c(mm,dd,pref47.tmp))
 }
 
-writeMatrix(xxx.infected,dir="data", col=TRUE, force=TRUE)
-writeMatrix(xxx.recovered,dir="data", col=TRUE, force=TRUE)
-writeMatrix(xxx.dead,dir="data", col=TRUE, force=TRUE)
+writeMatrix(pref47.infected,dir="data", col=TRUE, force=TRUE)
+writeMatrix(pref47.recovered,dir="data", col=TRUE, force=TRUE)
+writeMatrix(pref47.dead,dir="data", col=TRUE, force=TRUE)
 
 pjj_kinki4 <- pid[c("京都府","大阪府","兵庫県","奈良県")]
-matplot(xxx.infected[,pjj_kinki4+2],type="b", pch=1:7,col=1:8,log="y")
+matplot(pref47.infected[,pjj_kinki4+2],type="b", pch=1:7,col=1:8,log="y")
